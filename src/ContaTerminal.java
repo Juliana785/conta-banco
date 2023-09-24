bde278fce5bb79c5e3d404203dee7ba171313068
Juliana Capovilla <capovilla.ttg@gmail.com>
2023-09-24 00:58:41 -0300

    desafio conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,40 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+     public class ContaTerminal { 
+        /**
+         * @param args
+         */
+        public static void main(String[] args) {
+            
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Número da Conta:  ");
+            int conta = scanner.nextInt();
+
+            System.out.println("Número da agência:  ");
+            String agencia = scanner.next();
+
+            System.out.println("Nome do cliente:  ");
+            String nomeCliente = scanner.next();
+
+             System.out.println("Saldo:  ");
+             double saldo = scanner.nextDouble();
+
+              System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco" );
+              System.out.println("Agência" + agencia);
+              System.out.println("Número da conta:" + conta);
+              System.out.println("Saldo" + saldo + "Disponível para saque");
+
+
+
+
+
+
+
+
+        }
     }
-}
